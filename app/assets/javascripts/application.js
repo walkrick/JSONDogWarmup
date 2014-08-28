fb6a2bca344f0f942bc36a8f04124046f691f358
Rick Walker <walkrick@yahoo.com>
2014-08-28 13:39:47 -0600

    updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,5 +45,13 @@ $(document).ready(function () {
     ]
   };
   // Write come code to add the dogs to the table !!!!
+  var rows = "";
+  $(gschoolDogs.dogs).each(function(){
+    rows += "<tr><td>" + this.owner + "</td><td>" + this.name + "</td><td>" + this.preferences[0].favoriteFood + "</td><td>" + this.preferences[1].favoriteToy + "</td></tr>";
+  });
+
+  $(rows).appendTo('#dogs')
+
+
 });
 
